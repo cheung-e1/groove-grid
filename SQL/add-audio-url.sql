dc698ff358e86f545ce934ad7430b7047176a702
cheung-e1 <cheung-e1@ulster.ac.uk>
2024-12-08 01:28:03 +0000

    Create sql query to add image_url and audio_url columns to albums and tracks table

---
+++ SQL/add-audio-url.sql
@@ -0,0 +1,3 @@
+-- Add a column for audio_url in the tracks table
+ALTER TABLE tracks
+ADD audio_url VARCHAR(500);
---
+++ SQL/add-image-url.sql
@@ -0,0 +1,3 @@
+-- Add a column for image_url in the albums table
+ALTER TABLE albums
+ADD image_url VARCHAR(500);
